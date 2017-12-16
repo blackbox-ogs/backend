429f83779034c2c78593177c051d044a5f64f9d4
Reef Loretto <reefloretto@gmail.com>
2017-12-15 19:35:15 -0500

    add api to path

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/tracks', tracks);
+app.use('/api/tracks', tracks);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
